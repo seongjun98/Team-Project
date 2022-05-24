7deb466b46471d85d437b9d2cc538398806217e3
Jeonhui <dlwjsgml02@naver.com>
2022-05-24 17:04:44 +0900

    update heart

--- Method.py
+++ Method.py
@@ -4,6 +4,7 @@ from turtle import done
 import pygame
 import random
 import bomb
+import person
 
 # 게임판 구성
 pygame.init()
@@ -14,24 +15,10 @@ screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 
 bomb.init(SCREEN_WIDTH, SCREEN_HEIGHT)
+person.init(SCREEN_WIDTH, SCREEN_HEIGHT)
 
 # 배경이미지
 # background = pygame.image.load("background.png")
-
-# 캐릭터
-character = pygame.transform.scale(pygame.image.load("src/character.png"), (50,50))
-characterSize = character.get_rect().size  # img크기 불러옴
-characterWidth = characterSize[0]
-characterHeight = characterSize[1]
-characterXpos = (SCREEN_WIDTH / 2) - (characterWidth / 2)
-characterYpos = SCREEN_HEIGHT - characterHeight
-
-# 이동할 좌표
-toX = 0
-toY = 0
-
-# 이동속도
-characterSpeed = 10
 
 # 난수 생성 - 똥 생성용
 randomNumber = 30
@@ -41,10 +28,17 @@ poSpeed = 10
 totalTime = 10
 startTicks = pygame.time.get_ticks()
 
+heartfont = pygame.font.SysFont("arial", 30, True, True)
+heart3 = heartfont.render("♥ ♥ ♥", True, (0, 0, 0))
+heart2 = heartfont.render("  ♥ ♥", True, (0, 0, 0))
+heart1 = heartfont.render("    ♥", True, (0, 0, 0))
+
 
 def runGame():
-    global toX, toY, characterXpos, characterYpos
     run = True
+
+    Gaming = True  # 시작 버튼을 눌렀을 경우
+    heart = 3  # 시작 버튼을 눌렀을 경우
 
     while run:
         screen.fill((255, 255, 255))
@@ -53,44 +47,45 @@ def runGame():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    toX -= characterSpeed
-                if event.key == pygame.K_RIGHT:
-                    toX += characterSpeed
-                if event.key == pygame.K_UP:
-                    toY -= characterSpeed
-                if event.key == pygame.K_DOWN:
-                    toY += characterSpeed
-            if event.type == pygame.KEYUP:
-                if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
-                    toX = 0
-                elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
-                    toY = 0
+            if Gaming:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        person.moveX(-1)
+                    if event.key == pygame.K_RIGHT:
+                        person.moveX(1)
+                    if event.key == pygame.K_UP:
+                        person.moveY(-1)
+                    if event.key == pygame.K_DOWN:
+                        person.moveY(1)
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                        person.toX = 0
+                    elif event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                        person.toY = 0
+        if Gaming:
+            bomb.run(screen)
+            person.run(screen)
+            if heart == 3:
+                screen.blit(heart3, (500, 0))
+            elif heart == 2:
+                screen.blit(heart2, (500, 0))
+            elif heart == 1:
+                screen.blit(heart1, (500, 0))
 
-
-
-        # 캐릭터 이동 & 프레임맞추기
-        characterXpos += toX
-        characterYpos += toY
-
-        # 경계 설정-가로
-        if characterXpos < 0:
-            characterXpos = 0
-        elif characterXpos > SCREEN_WIDTH - characterWidth:
-            characterXpos = SCREEN_WIDTH - characterWidth
-
-        # 경계 설정-세로
-        if characterYpos < 0:
-            characterYpos = 0
-        elif characterYpos > SCREEN_HEIGHT - characterHeight:
-            characterYpos = SCREEN_HEIGHT - characterHeight
-
-        screen.blit(character, (characterXpos, characterYpos))
-
-        bomb.run(screen)
+            pp = person.getPos()
+            for b in bomb.explosion:
+                if b['hit']:
+                    continue
+                if (b['rect'].left <= pp['x'] <= b['rect'].left + b['scale']) and (
+                        b['rect'].top <= pp['y'] <= b['rect'].top + b['scale']):
+                    heart -= 1
+                    b['hit'] = True
+                    if heart <= 0:
+                        Gaming = False
+                    break
 
         pygame.display.update()
+
     pygame.quit()
 
 
